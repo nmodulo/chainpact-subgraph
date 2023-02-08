ba1730b85da0b412d23204500b20115e99a2c2da
Sayan Samanta <samantasayan23@gmail.com>
2023-02-09 02:40:34 +0530

    initial

---
+++ src/chainpact.ts
@@ -0,0 +1,54 @@
+import {
+  LogPactCreated as LogPactCreatedEvent,
+  LogPaymentMade as LogPaymentMadeEvent
+} from "../generated/test/GigPact"
+import { ProposalPact, logPactCreated as LogProposalPactCreatedEvent } from "../generated/proposalpact/ProposalPact"
+import { LogPactCreated, LogPaymentMade, LogProposalPactCreated } from "../generated/schema"
+import { TypedMap } from '@graphprotocol/graph-ts'
+
+export function handleLogPactCreated(event: LogPactCreatedEvent): void {
+  let entity = new LogPactCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.creator = event.params.creator
+  entity.pactId = event.params.pactid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLogPaymentMade(event: LogPaymentMadeEvent): void {
+  let entity = new LogPaymentMade(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.payer = event.params.payer
+  entity.pactId = event.params.pactid
+  entity.value = event.params.value
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleLogProposalPactCreated(event: LogProposalPactCreatedEvent): void {
+  let entity = new LogProposalPactCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32()) 
+  )
+  let contract = ProposalPact.bind(event.address)
+  let pactInfoFromChain = contract.pacts(event.params.uid)
+
+  entity.creator = event.params.creator
+  entity.uid = event.params.uid
+  entity.isEditable = pactInfoFromChain.getIsEditable()
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
