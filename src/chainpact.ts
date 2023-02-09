a60fc501c40de755a39f9381b185a11b0cd16de8
Sayan Samanta <samantasayan23@gmail.com>
2023-02-09 21:11:28 +0530

    added test code

--- src/chainpact.ts
+++ src/chainpact.ts
@@ -3,8 +3,8 @@ import {
   LogPaymentMade as LogPaymentMadeEvent
 } from "../generated/gigpact/GigPact"
 import { ProposalPact, logPactCreated as LogProposalPactCreatedEvent } from "../generated/proposalpact/ProposalPact"
-import { LogPactCreated, LogPaymentMade, LogProposalPactCreated } from "../generated/schema"
-import { TypedMap } from '@graphprotocol/graph-ts'
+import { LogPactCreated, LogPaymentMade, LogProposalPactCreated, VotingInfo } from "../generated/schema"
+import { ByteArray, Bytes, TypedMap } from '@graphprotocol/graph-ts'
 
 export function handleLogPactCreated(event: LogPactCreatedEvent): void {
   let entity = new LogPactCreated(
@@ -12,6 +12,7 @@ export function handleLogPactCreated(event: LogPactCreatedEvent): void {
   )
   entity.creator = event.params.creator
   entity.pactId = event.params.pactid
+  console.log("hey there")
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
@@ -39,12 +40,31 @@ export function handleLogProposalPactCreated(event: LogProposalPactCreatedEvent)
   let entity = new LogProposalPactCreated(
     event.transaction.hash.concatI32(event.logIndex.toI32()) 
   )
+  let votingInfoEntity = new VotingInfo(event.transaction.hash.concatI32(event.logIndex.toI32()))
   let contract = ProposalPact.bind(event.address)
   let pactInfoFromChain = contract.pacts(event.params.uid)
+  let votingInfoFromChain = contract.votingInfo(event.params.uid)
 
   entity.creator = event.params.creator
   entity.uid = event.params.uid
   entity.isEditable = pactInfoFromChain.getIsEditable()
+  entity.pactText = pactInfoFromChain.getPactText()
+  entity.refundAvailable = pactInfoFromChain.getRefundAvailable()
+  entity.totalValue = pactInfoFromChain.getTotalValue().toString()
+  entity.yesVotes = pactInfoFromChain.getYesVotes()
+  entity.noVotes = pactInfoFromChain.getNoVotes()
+  console.log("hey there")
+
+  votingInfoEntity.votingEnabled = votingInfoFromChain.getVotingEnabled()
+  votingInfoEntity.openParticipation = votingInfoFromChain.getOpenParticipation()
+  votingInfoEntity.refundOnVotedYes = votingInfoFromChain.getRefundOnVotedYes()
+  votingInfoEntity.refundOnVotedNo = votingInfoFromChain.getRefundOnVotedNo()
+  votingInfoEntity.votingConcluded = votingInfoFromChain.getVotingConcluded()
+  votingInfoEntity.duration = votingInfoFromChain.getDuration()
+  votingInfoEntity.votingStartTimestamp = votingInfoFromChain.getVotingStartTimestamp()
+  votingInfoEntity.minContribution = votingInfoFromChain.getMinContribution().toString()
+  
+  // entity.votingInfo = votingInfoEntity
 
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
--- tests/test-utils.ts
+++ tests/test-utils.ts
@@ -1,51 +1,78 @@
 import { newMockEvent } from "matchstick-as"
-import { ethereum, Address } from "@graphprotocol/graph-ts"
-import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/test/test"
+import { ethereum, Address, Bytes } from "@graphprotocol/graph-ts"
+// import { AdminChanged, BeaconUpgraded, Upgraded } from "../generated/test/test"
+import {
+  LogPactCreated as LogPactCreatedEvent,
+  LogPaymentMade as LogPaymentMadeEventpactCreatedEvent
+} from "../generated/gigpact/GigPact"
+import { ProposalPact, logPactCreated as LogProposalPactCreatedEvent } from "../generated/proposalpact/ProposalPact"
 
-export function createAdminChangedEvent(
-  previousAdmin: Address,
-  newAdmin: Address
-): AdminChanged {
-  let adminChangedEvent = changetype<AdminChanged>(newMockEvent())
+// export function createAdminChangedEvent(
+//   previousAdmin: Address,
+//   newAdmin: Address
+// ): AdminChanged {
+//   let adminChangedEvent = <AdminChanged>(newMockEvent())
 
-  adminChangedEvent.parameters = new Array()
+//   adminChangedEvent.parameters = new Array()
 
-  adminChangedEvent.parameters.push(
+  // adminChangedEvent.parameters.push(
+  //   new ethereum.EventParam(
+  //     "previousAdmin",
+  //     ethereum.Value.fromAddress(previousAdmin)
+  //   )
+  // )
+  // adminChangedEvent.parameters.push(
+  //   new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
+  // )
+
+  // return adminChangedEvent
+// }
+
+// export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
+//   let beaconUpgradedEvent = <BeaconUpgraded>(newMockEvent())
+
+//   beaconUpgradedEvent.parameters = new Array()
+
+//   beaconUpgradedEvent.parameters.push(
+//     new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
+//   )
+
+//   return beaconUpgradedEvent
+// }
+
+// export function createUpgradedEvent(implementation: Address): Upgraded {
+//   let upgradedEvent = <Upgraded>(newMockEvent())
+
+//   upgradedEvent.parameters = new Array()
+
+//   upgradedEvent.parameters.push(
+//     new ethereum.EventParam(
+//       "implementation",
+//       ethereum.Value.fromAddress(implementation)
+//     )
+//   )
+
+//   return upgradedEvent
+// }
+
+export function createLogPactCreatedEvent(
+  creator: Address,
+  pactId: Bytes
+) {
+  let pactCreatedEvent = <LogPactCreatedEvent>(newMockEvent())
+  pactCreatedEvent.parameters = new Array()
+  pactCreatedEvent.parameters.push(
     new ethereum.EventParam(
-      "previousAdmin",
-      ethereum.Value.fromAddress(previousAdmin)
+      "creator",
+      ethereum.Value.fromAddress(creator)
     )
   )
-  adminChangedEvent.parameters.push(
-    new ethereum.EventParam("newAdmin", ethereum.Value.fromAddress(newAdmin))
-  )
-
-  return adminChangedEvent
-}
-
-export function createBeaconUpgradedEvent(beacon: Address): BeaconUpgraded {
-  let beaconUpgradedEvent = changetype<BeaconUpgraded>(newMockEvent())
-
-  beaconUpgradedEvent.parameters = new Array()
-
-  beaconUpgradedEvent.parameters.push(
-    new ethereum.EventParam("beacon", ethereum.Value.fromAddress(beacon))
-  )
-
-  return beaconUpgradedEvent
-}
-
-export function createUpgradedEvent(implementation: Address): Upgraded {
-  let upgradedEvent = changetype<Upgraded>(newMockEvent())
-
-  upgradedEvent.parameters = new Array()
-
-  upgradedEvent.parameters.push(
+  pactCreatedEvent.parameters.push(
     new ethereum.EventParam(
-      "implementation",
-      ethereum.Value.fromAddress(implementation)
+      "pactid",
+      ethereum.Value.fromAddress(pactId)
     )
   )
 
-  return upgradedEvent
+  return pactCreatedEvent
 }
